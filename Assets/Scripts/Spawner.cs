58169a5f5653aae31529d1c9a5a04e129087348a
Makushi <maxivicraba@hotmail.com>
2017-05-02 20:28:20 -0300

    Initial Commit

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject target;
+	public GameObject spawnPoints;
+	float timer = 0.0f;
+
+	void Update () 
+	{
+		timer += Time.deltaTime;
+
+		if (timer > 2.0f) 
+		{
+			timer = 0.0f;
+			Vector3 spawnPoint = spawnPoints.transform.GetChild (Random.Range (0, spawnPoints.transform.childCount)).position;
+			//Vector3 prefabPosition = new Vector3 (Random.Range(-5,6), Random.Range(0,6), 10);
+			Instantiate (target, spawnPoint , Quaternion.identity);
+		}
+	}
+}
