7cb7ae20e3c97a7bb84bf97604b0ae15e2e1b490
Makushi <maxivicraba@hotmail.com>
2017-05-02 22:20:08 -0300

    Gun aim

--- Assets/Scripts/MouseHandler.cs
+++ Assets/Scripts/MouseHandler.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class MouseHandler : MonoBehaviour {
 
+	private float horinzontalSpeed = 2.0F;
+	private float verticalSpeed = 2.0F;
+
+	void Update()
+	{
+		Vector3 mousePos = Input.mousePosition;
+		transform.LookAt(Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, Screen.height - mousePos.y, 10.0f)));
+	}		
+		
 	void OnMouseDown()
 	{
-		Destroy (gameObject);
+		Animator anim = transform.GetComponent<Animator> ();
 	}
 }
